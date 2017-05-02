005250eb77bfb809106c04584a6b41973310a431
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-02 14:14:29 +0800

    Updated test.

--- plough-test.js
+++ plough-test.js
@@ -1,17 +1,20 @@
 "use strict";
 
+const assert = require( "assert" );
 const plough = require( "./plough.js" );
 
-console.log( plough( [ 1, 2, 3, [ 4, 5, 6 ] ] ) );
-console.log( plough( [ ] ) );
-console.log( plough( ) );
+assert.ok( plough( [ 1, 2, 3, [ 4, 5, 6 ] ] ) );
+assert.ok( plough( [ ] ) );
+assert.ok( plough( ) );
 
 
 ( function ( ){
-	console.log( plough( arguments ) );
+	assert.ok( plough( arguments ) );
 } )( ["hello", ["world"],1,2,3], ["yeah",4,5,6] );
 
 
 ( function ( ){
-	console.log( plough( arguments ) );
+	assert.ok( plough( arguments ) );
 } )( );
+
+console.log( "ok" );
