1ac719b85eed512cff9cf8a9df87687a44f07724
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-25 13:13:29 +0800

    Updated test.

--- plough-test.js
+++ plough-test.js
@@ -2,18 +2,16 @@
 const assert = require( "assert" );
 const plough = require( "./plough.js" );
 
-assert.deepEqual( plough( ), [ ], "should be empty array" );
+assert.deepEqual( plough( ), [ ], "should return [ ]" );
 
-assert.deepEqual( plough( [ ] ), [ ], "should be empty array" );
+assert.deepEqual( plough( [ ] ), [ ], "should return [ ]" );
 
-assert.deepEqual( plough( [ 1, 2, 3, [ 4, 5, 6 ] ] ), [ 1, 2, 3, 4, 5, 6 ], "should be equal" );
-
-( function ( ){
-	assert.deepEqual( plough( arguments ), [ ], "should be empty array" );
-} )( );
+assert.deepEqual( plough( [ 1, 2, 3, [ 4, 5, 6 ] ] ), [ 1, 2, 3, 4, 5, 6 ],
+	"should return [ 1, 2, 3, 4, 5, 6 ]" );
 
 ( function ( ){
-	assert.deepEqual( plough( arguments ), [ "hello", "world", 1, 2, 3, "yeah", 4, 5, 6 ], "should be equal" );
+	assert.deepEqual( plough( arguments ), [ "hello", "world", 1, 2, 3, "yeah", 4, 5, 6 ],
+	"should return [ 'hello', 'world', 1, 2, 3, 'yeah', 4, 5, 6 ]" );
 } )( [ "hello", [ "world" ], 1, 2, 3 ], [ "yeah", 4, 5, 6 ] );
 
 console.log( "ok" );
